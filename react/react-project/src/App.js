ed5cdd7ad11a7f2f9377421cba41f9cb1084cd6b
zhala <fill_flls@icloud.com>
2025-01-03 19:50:16 +0100

    обнавоено папка src добавлено новый файл banktermin

--- react/react-project/src/App.js
+++ react/react-project/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Konverter from './pages/konverter';
 import Calculator from './pages/calc';
+import Terminal from './pages/banktermin';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
             <Route path='/' element={<Calculator/>} />
             <Route path='/konverter' element={<Konverter/>} />
+            <Route path='/banktermin' element={<Terminal/>} />
         </Routes>
       </BrowserRouter>
     </div>
